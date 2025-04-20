5b1e5151752f9dbdf4aa78dd1237b8077acd7159
Artem Sokolov <smbrer1@gmail.com>
2025-04-20 22:32:24 +0300

    finished

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+# Логирование в файл
+logging.basicConfig(filename='/var/log/flask-app.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.route('/')
+def home():
+    logger.info('Received request on home page')
+    return 'Hello, Monitoring!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
